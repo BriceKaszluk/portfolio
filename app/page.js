48f27dbd49ccd55643f6f067d6050bd986b00ce7
BriceKaszluk <kaszlukb@gmail.com>
2023-11-08 08:52:49 +0100

    feat: modify metadata

--- app/page.js
+++ app/page.js
@@ -1,40 +1,9 @@
 import Image from "next/image";
-import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>
-          Brice - Développeur Full Stack Spécialisé en React | Portfolio
-        </title>
-        <meta
-          name="description"
-          content="Découvrez le portfolio de Brice, développeur Full Stack spécialisé en React, Node.js, et technologies modernes du web. Passionné et prêt à relever de nouveaux défis."
-        />
-        <meta
-          name="keywords"
-          content="Brice, Développeur Full Stack, React, Next.js, Node.js, Supabase, PostgreSQL, Firebase, Material UI, Tailwind CSS, AWS, Développement Web, Portfolio"
-        />
-        <link rel="canonical" href="https://votre-domaine.com" />
-        <meta
-          property="og:title"
-          content="Brice - Développeur Full Stack Spécialisé en React | Portfolio"
-        />
-        <meta
-          property="og:description"
-          content="Un développeur animé par une passion pour le web et la création d'expériences utilisateurs captivantes. Découvrez mon parcours et mes projets."
-        />
-        <meta property="og:image" content="https://portfolio-og.png" />
-        <meta property="og:url" content="https://votre-domaine.com" />
-        <meta property="og:site_name" content="Portfolio de Brice" />
-        {/* Ajoutez le fichier favicon.ico dans le dossier public */}
-        <link rel="icon" href="/favicon.ico" />
-        {/* Si vous avez un compte LinkedIn, vous pouvez ajouter le lien ici */}
-        <link rel="me" href="https://www.linkedin.com/in/bricekaszluk" />
-        <meta name="author" content="Brice, kaszlukb@gmail.com" />
-      </Head>
       <div className="flex h-full flex-col md:flex-row min-h-screen">
         {/* Colonne de gauche */}
         <div className="md:w-1/4 p-4 flex flex-col justify-around text-center">
